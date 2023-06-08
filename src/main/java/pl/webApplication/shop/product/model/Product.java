a291cfd574742843269b90a5c97a36a9fe22e595
Sebastian <sebastian.kuropatwa.it@gmail.com>
2023-06-08 21:52:24 +0200

    feature(backend): the first REST service

---
+++ src/main/java/pl/webApplication/shop/product/model/Product.java
@@ -0,0 +1,17 @@
+package pl.webApplication.shop.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+public class Product {
+
+    private String name;
+    private String category;
+    private String description;
+    private BigDecimal price;
+    private String currency;
+}
